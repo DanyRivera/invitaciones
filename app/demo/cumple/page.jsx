b129e65efac2a1bbc4a20849c7402cfed42f7b7b
Dany Rivera <dany@gmail.com>
2023-08-16 21:39:05 -0600

    Trabajando en el header

--- app/demo/cumple/page.jsx
+++ app/demo/cumple/page.jsx
@@ -4,7 +4,7 @@ export const Page = () => {
     return (
         <>
             <header className="relative">
-                <div className="grid md:grid-cols-2 w-full h-full relative">
+                <div className="grid md:grid-cols-2 w-full h-full">
                     <Image
                         src="https://firebasestorage.googleapis.com/v0/b/invitacion-8e824.appspot.com/o/demo%2Fcumple%2Fh-1.webp?alt=media&token=5af6ee5c-2112-405b-b0ef-b679142a222c"
                         width={500}
@@ -34,7 +34,7 @@ export const Page = () => {
                         className="w-full"
                     />
                 </div>
-                <div className="absolute">
+                <div className="absolute top-[50%] left-[50%] bg-[#fff]">
                     <h1>Te invito a mi cumpleaños!</h1>
                     <h2>Luis González</h2>
                 </div>
