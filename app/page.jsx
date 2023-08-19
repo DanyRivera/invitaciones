551b913d26467bc5c33453f8512b729ad77f2128
Dany <dany@gmail.com>
2023-08-19 11:31:15 -0600

    Redireccionando a la landing

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,19 @@
+'use client'
+
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('https://miinvitacion.vercel.app')
+  }, [])
+
   return (
-    <div>Hola</div>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-8xl tracking-widest font-bold text-[#7bdff2]">Byeeee!</h1>
+    </div>
   )
 }
